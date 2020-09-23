d7d0f7bfe2176915322b312e9caba78b3eb7d5e2
HeroWong <hero_wong@outlook.com>
2020-09-23 20:24:32 +0800

    test: test for DateTime

--- DogHappy.AspNet.Echinocactus.Tests/ExtensionTests/DataTableExtensionTests/CellValueTest.cs
+++ DogHappy.AspNet.Echinocactus.Tests/ExtensionTests/DataTableExtensionTests/CellValueTest.cs
@@ -1,5 +1,6 @@
 ﻿using DogHappy.AspNet.Echinocactus.Extensions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Data;
 
 namespace DogHappy.AspNet.Echinocactus.Tests.ExtensionTests.DataTableExtensionTests
@@ -16,18 +17,22 @@ namespace DogHappy.AspNet.Echinocactus.Tests.ExtensionTests.DataTableExtensionTe
             table.Columns.Add("Age", typeof(int));
             table.Columns.Add("Gender", typeof(bool));
             table.Columns.Add("IsDeleted", typeof(bool));
+            table.Columns.Add("CreatedAt", typeof(DateTime));
 
             var row = table.NewRow();
             row["Id"] = 1;
             row["Name"] = "doghappy";
             row["Age"] = 18;
             row["IsDeleted"] = false;
+            DateTime createdAt = DateTime.Parse("2020-09-19 10:49:27");
+            row["CreatedAt"] = createdAt;
 
             Assert.AreEqual(1, row.GetCellValue<int>("Id"));
             Assert.AreEqual("doghappy", row.GetCellValue<string>("Name"));
             Assert.AreEqual(18, row.GetCellValue<int?>("Age").Value);
             Assert.IsFalse(row.GetCellValue<int?>("Gender").HasValue);
             Assert.IsFalse(row.GetCellValue<bool>("IsDeleted"));
+            Assert.AreEqual(createdAt.ToString("yyyy-MM-dd HH:mm:ss"), row.GetCellValue<DateTime>("CreatedAt").ToString("yyyy-MM-dd HH:mm:ss"));
         }
     }
 }
